1f9612056f50e5029a2ba34814c6f4599834ebb6
Shoohag <shohag.sgazi@gmail.com>
2023-06-01 15:53:57 +0600

    cart button add

---
+++ src/Product/Product.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    // console.log(props)
+    const {img,name, seller, ratings, price} = props.product;
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='products-info'>
+                <h6 className='product-name'>{name}</h6>
+                <p>Price: ${price}</p>
+                <p>Manufacturer: {seller}</p>
+                <p>Ratings: {ratings} Star</p>
+            </div>
+            <button className='btn-cart'>Add to Cart</button>
+        </div>
+    );
+};
+
+export default Product;
